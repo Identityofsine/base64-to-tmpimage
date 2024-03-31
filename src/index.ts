60f10c3c0ce19a5981500c05340a527afbc72b2d
fofx <identityofsine@gmail.com>
2024-03-30 23:54:23 -0400

    added apis

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,49 @@
 import express from 'express';
 import { TemporaryFile } from './tempfile';
+import path from 'path';
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+//env
+require('dotenv').config();
+
+try {
+	TemporaryFile.createTempFolder();
+	TemporaryFile.deleteTempFiles();
+} catch (e) {
+	console.error(e);
+}
 
 const app = express();
 app.use(express.json({ limit: '50mb' }));
 
-app.get('/', (req, res) => {
-	res.send('Hello World');
+app.get('/:file', (req, res) => {
+	try {
+		// @ts-ignore
+		const file = req.params!.file;
+		const dirname = path.resolve();
+		res.status(200).sendFile('./temp/' + file, { root: dirname }, function(err) {
+			if (err) {
+				res.status(404).json({ error: 'File not found' }).send;
+			}
+		});
+	} catch (e: any) {
+		res.status(500).json({ error: e }).send();
+	}
 });
 
 app.post('/', (req, res) => {
-	const { file } = req.body;
-	const extension = TemporaryFile.detectExtensionImage(file);
-	console.log(extension);
-
-	res.sendStatus(200);
-
+	try {
+		const { file } = req.body;
+		const extension = TemporaryFile.detectExtensionImage(file);
+		const temp_file = TemporaryFile.createFromBase64(file, extension);
+		const path = temp_file?.getFileName();
+		const expiration = new Date(Date.now() + 30 * 60000);
+		res.status(203).json({ path: path, expiration: `${expiration.getTime()}` }).send();
+	} catch (e) {
+		console.error(e);
+		res.status(500).json({ error: e }).send();
+	}
 });
 
 app.listen(3000, () => {
--- src/tempfile.ts
+++ src/tempfile.ts
@@ -7,11 +7,14 @@ export class TemporaryFileError extends Error {
 	}
 }
 
+const base_url = process.env.BASE_URL || 'http://localhost:80';
+
 export class TemporaryFile {
 
 	path: string = "";
 	active: boolean = true;
 	deleteTimer: NodeJS.Timeout | undefined;
+	fileName: string = "";
 
 	/**
 		* @param content {Buffer} - The content of the filem should be a buffer
@@ -24,8 +27,9 @@ export class TemporaryFile {
 
 	private createFile(): void {
 		try {
+			this.fileName = `${new Date().getTime()}${this.extension}`;
+			this.path = `./temp/${this.fileName}`;
 			fs.writeFileSync(this.path, this.content);
-			this.path = `./temp/${new Date().getTime()}${this.extension}`;
 			this.startDeleteTimer();
 		} catch (err) {
 			throw new TemporaryFileError(`Error creating the file (${err})`);
@@ -42,6 +46,7 @@ export class TemporaryFile {
 		try {
 			fs.unlinkSync(this.path);
 			this.active = false;
+			console.log(`File ${this.fileName} deleted`);
 			this.onDeleted?.();
 		} catch (err) {
 			throw new TemporaryFileError(`Error deleting the file (${err})`);
@@ -59,23 +64,59 @@ export class TemporaryFile {
 		* @returns {string} - The path of the file
 	*/
 	public getPath(): string {
-		return '';
+		return this.path;
+	}
+
+	/**
+		* @returns {string} - URL path of the file
+	*/
+	public getFileName(): string {
+		return base_url + '/' + this.fileName;
+	}
+
+	public static createTempFolder(): void {
+		const path = './temp';
+		try {
+			if (!fs.existsSync(path)) {
+				fs.mkdirSync(path);
+			}
+		} catch (err) {
+			throw new TemporaryFileError(`Error creating the temp folder (${err})`);
+		}
+	}
+
+	public static deleteTempFiles(): void {
+		const path = './temp';
+		try {
+			fs.readdir(path, (err, files) => {
+				if (err) {
+					return;
+				}
+				files.forEach(file => {
+					fs.unlinkSync(`${path}/${file}`);
+				});
+			});
+		} catch (err) {
+			throw new TemporaryFileError(`Error deleting the files (${err})`);
+		}
 	}
 
 	public static detectExtensionImage(base64: string): string {
 		const base64Regex = /^data:image\/(.*?);base64/;
 		const extension = base64.replaceAll(" ", "").match(base64Regex);
 		if (extension) {
-			return `.found`;
+			return `.${extension?.[1]}`;
 		}
 		return '';
 	}
 
-
 	public static createFromBase64(base64: string, extension: string, ttk: number = 30, onDeleted?: () => void): TemporaryFile {
-		const buffer = Buffer.from(base64, 'base64');
+		const base64Regex = /^data:image\/(.*?);base64,/;
+		const buffer = Buffer.from(base64.replace(base64Regex, ''), 'base64');
+
 		return new TemporaryFile(buffer, extension, ttk, onDeleted);
 	}
+
 }
 
 
